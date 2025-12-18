8ea0fb7f0aa974d8447524fa6eba979776f11c01
Salmakhaled099 <salmalilo153@gmail.com>
2025-12-18 15:31:43 +0200

    Delete models/StraightProjectile.java

--- models/StraightProjectile.java
+++
@@ -1,7 +0,0 @@
-package com.example.battlearena.models;
-
-public class StraightProjectile extends Projectile {
-    public StraightProjectile(double x, double y, double vx, double vy, Weapon weapon, Fighter fighter, int owner) {
-        super(x, y, vx, vy, weapon, fighter, owner);
-    }
-}

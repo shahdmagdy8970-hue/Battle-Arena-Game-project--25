1be278e2b387c915aa9a2aeaa5aec63b8133f269
LILOOO20 <alaasherif639@gmail.com>
2025-12-19 01:12:35 +0200

    Update Renderer.java

--- utils/Renderer.java
+++ utils/Renderer.java
@@ -5,6 +5,7 @@ import com.example.battlearena.models.Projectile;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import java.util.List;
 
 public class Renderer {
@@ -22,12 +23,13 @@ public class Renderer {
 
     public void renderGame(GraphicsContext gc, Fighter player1, Fighter player2,
                            List<Projectile> projectiles, boolean fullMovement) {
-        gc.setFill(Color.rgb(26, 26, 46));
+
+        gc.setFill(Color.rgb(26, 26, 46, 0.6));
         gc.fillRect(0, 0, canvasWidth, canvasHeight);
 
         if (!fullMovement) {
             gc.setStroke(Color.WHITE);
-            gc.setLineWidth(2);
+            gc.setLineWidth(3);
             gc.setLineDashes(10, 10);
             gc.strokeLine(canvasWidth / 2.0, 0, canvasWidth / 2.0, canvasHeight);
             gc.setLineDashes(0);
@@ -40,6 +42,11 @@ public class Renderer {
             gc.setFill(p.getWeapon().getColor());
             gc.fillOval(p.getX() - projectileSize / 2.0, p.getY() - projectileSize / 2.0,
                     projectileSize, projectileSize);
+
+            gc.setStroke(Color.WHITE);
+            gc.setLineWidth(2);
+            gc.strokeOval(p.getX() - projectileSize / 2.0, p.getY() - projectileSize / 2.0,
+                    projectileSize, projectileSize);
         }
     }
 
@@ -51,13 +58,25 @@ public class Renderer {
         gc.setFill(f.getColor());
         gc.fillRect(-playerSize / 2.0, -playerSize / 2.0, playerSize, playerSize);
 
+        gc.setStroke(Color.WHITE);
+        gc.setLineWidth(3);
+        gc.strokeRect(-playerSize / 2.0, -playerSize / 2.0, playerSize, playerSize);
+
         gc.setFill(Color.WHITE);
         gc.fillRect(playerSize / 4.0, -3, playerSize / 4.0, 6);
 
         gc.restore();
 
         gc.setFill(Color.WHITE);
-        gc.setFont(Font.font(12));
-        gc.fillText(f.getType().getDisplayName(), f.getX() - 20, f.getY() + playerSize / 2.0 + 15);
+        gc.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(3);
+
+        String name = f.getType().getDisplayName();
+        double textX = f.getX() - 25;
+        double textY = f.getY() + playerSize / 2.0 + 20;
+
+        gc.strokeText(name, textX, textY);
+        gc.fillText(name, textX, textY);
     }
 }
